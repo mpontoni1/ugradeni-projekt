b1a202a5ad76c6b18ac84d042f0edce62dd24b20
mpontoni1 <ponmarin555@gmail.com>
2025-06-17 16:44:01 +0200

    Update

--- main/ugradeni-projekt.c
+++ main/ugradeni-projekt.c
@@ -4,6 +4,26 @@
 #include "freertos/task.h"
 #include "driver/adc.h"
 #include "dht11.h"
+#include "driver/i2c.h"
+
+// MQ135 digital pin
+#define MQ_digital GPIO_NUM_13
+// MQ135 analog pin
+#define MQ_analog ADC1_CHANNEL_3
+
+// I2C prvi uredaj -> zato sto nema niti jednog drugog onda je on prvi
+#define I2C_PORT I2C_NUM_0
+// Data i clock pin-ovi
+#define I2C_SDA 21
+#define I2C_SCL 22
+// Frekvencija komunikacije
+#define I2C_FREQ 100000
+// Adresa display-a, defaultna = 0x27
+#define LCD_ADDR 0x27
+
+#define LCD_BACKLIGHT 0x08
+#define LCD_ENABLE 0x04
+#define LCD_RS 0x01
 
 // delay funkcija
 
@@ -12,12 +32,82 @@ void delay(int ms)
     vTaskDelay(ms / portTICK_PERIOD_MS);
 }
 
+void lcd_write(uint8_t data)
+{
+    // Uspostavi i2c vezu
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    // Zapocni zapis u cmd sekvencu
+    i2c_master_start(cmd);
+    // Shiftaj adresu lcd-a u lijevo i zapisi (0 je write bit), true -> ocekuj odgovor od uredaja
+    i2c_master_write_byte(cmd, (LCD_ADDR << 1) | 0, true);
+    // Zapisi potreban data na uredaj
+    i2c_master_write_byte(cmd, data, true);
+    // Zaustavi zapis u cmd sekvencu
+    i2c_master_stop(cmd);
+    // Zapocni komunikaciju
+    i2c_master_cmd_begin(I2C_PORT, cmd, pdMS_TO_TICKS(50));
+    // Oslobodi memoriju nakon zavrsene komunikacije
+    i2c_cmd_link_delete(cmd);
+}
+
+// slanje 4 bita
+void lcd_send_nibble(uint8_t nib, uint8_t mode)
+{
+    uint8_t data = (nib & 0xF0) | mode | LCD_BACKLIGHT;
+    lcd_write(data | LCD_ENABLE);
+    esp_rom_delay_us(1);
+    lcd_write(data & ~LCD_ENABLE);
+    esp_rom_delay_us(50);
+}
+
+// slanje 8 bita
+void lcd_send_byte(uint8_t val, uint8_t mode)
+{
+    lcd_send_nibble(val & 0xF0, mode);
+    lcd_send_nibble((val << 4) & 0xF0, mode);
+}
+
+void lcd_cmd(uint8_t cmd)
+{
+    lcd_send_byte(cmd, 0);
+}
+
+void lcd_char(char c)
+{
+    lcd_send_byte(c, LCD_RS);
+}
+
+void lcd_init()
+{
+    vTaskDelay(pdMS_TO_TICKS(50));
+    lcd_send_nibble(0x30, 0);
+    vTaskDelay(pdMS_TO_TICKS(5));
+    lcd_send_nibble(0x30, 0);
+    esp_rom_delay_us(150);
+    lcd_send_nibble(0x30, 0);
+    lcd_send_nibble(0x20, 0); // 4-bit mode
+
+    lcd_cmd(0x28); // 4-bit, 2-line
+    lcd_cmd(0x0C); // display ON
+    lcd_cmd(0x06); // entry mode
+    lcd_cmd(0x01); // clear
+    vTaskDelay(pdMS_TO_TICKS(2));
+}
+
+void lcd_set_cursor(uint8_t col, uint8_t row)
+{
+    const uint8_t offsets[] = {0x00, 0x40};
+    lcd_cmd(0x80 | (col + offsets[row]));
+}
+
+void lcd_print(const char *s)
+{
+    while (*s)
+        lcd_char(*s++);
+}
+
 void app_main(void)
 {
-// MQ135 digital pin
-#define MQ_digital GPIO_NUM_13
-// MQ135 analog pin
-#define MQ_analog ADC1_CHANNEL_3
 
     int humidity = 0;
     int temperature = 0;
@@ -33,6 +123,23 @@ void app_main(void)
     // Ocitavaj raspon od 0 do 3.3v na analognom ulazu od MQ135
     adc1_config_channel_atten(MQ_analog, ADC_ATTEN_DB_11);
 
+    i2c_config_t cfg = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = I2C_SDA,
+        .scl_io_num = I2C_SCL,
+        .sda_pullup_en = true,
+        .scl_pullup_en = true,
+        .master.clk_speed = I2C_FREQ,
+    };
+    i2c_param_config(I2C_PORT, &cfg);
+    i2c_driver_install(I2C_PORT, cfg.mode, 0, 0, 0);
+
+    lcd_init();
+
+
+
+    
+
     while (1)
     {
         // Dohvati trenutnu analognu vrijednost od MQ135
@@ -42,8 +149,30 @@ void app_main(void)
         // Dohvati trenutnu vrijednost temperature i vlage sa DHT11
         temperature = DHT11_read().temperature;
         humidity = DHT11_read().humidity;
+        int airQ = (MQreadAnalog * 100)/4096;
+        char tempStr[3];
+        char humStr[2];
+        char airqStr[10];
 
-        printf("%d %d %d\n", temperature, humidity, MQreadAnalog);
+        sprintf(tempStr,"%d",temperature);
+        sprintf(humStr,"%d",humidity);
+        sprintf(airqStr,"%d",airQ);
+
+        printf("%s\n",airqStr);
+
+        lcd_set_cursor(0, 0);
+        lcd_print("Temp:");
+        lcd_print(tempStr);
+        lcd_set_cursor(9, 0);
+        lcd_print("Hum:");
+        lcd_print(humStr);
+        lcd_set_cursor(0,1);
+        lcd_print("Air Q: ");
+        lcd_set_cursor(7,1);
+        lcd_print(airqStr);
+        lcd_print("%");
+
+
         delay(500);
     }
 }
